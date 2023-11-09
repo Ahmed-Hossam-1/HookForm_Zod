12f092afa1e7bdb9a6afcba2779f990c7eb41d17
Ahmed Hossam <128134339+Ahmed-Hossam-1@users.noreply.github.com>
2023-11-09 17:13:55 +0200

    add zod

--- from_validation/src/App.tsx
+++ from_validation/src/App.tsx
@@ -1,35 +1,86 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useForm, SubmitHandler } from "react-hook-form";
+import z from "zod";
+import "./App.css";
+import { zodResolver } from "@hookform/resolvers/zod";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const signupSchema = z
+    .object({
+      first_name: z.string().min(1, { message: "First Name is required" }),
+      last_name: z.string().min(1, { message: "last Name is required" }),
+      email: z
+        .string()
+        .min(1, { message: "Email is required" })
+        .email({ message: "Not Vaild Email" }),
+      password: z
+        .string()
+        .min(6, { message: "password mast be at least 6 char" }),
+      confirm_password: z
+        .string()
+        .min(6, { message: "password mast be at least 6 char" }),
+    })
+    .refine((data) => data.password === data.confirm_password, {
+      message: "password do not match",
+      path: ["confirm_password"],
+    });
+
+  type ISignUp = z.infer<typeof signupSchema>;
+  const {
+    handleSubmit,
+    register,
+    formState: { errors, isSubmitting },
+  } = useForm<ISignUp>({
+    mode: "onChange",
+    resolver: zodResolver(signupSchema),
+  });
+  const onSubmit: SubmitHandler<ISignUp> = (data: ISignUp) => {
+    console.log(data);
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+      <h1>Create an Account</h1>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <input
+          type="text"
+          placeholder="Enter Your First Name"
+          {...register("first_name")}
+        />
+        {errors.first_name && <span>{errors.first_name.message}</span>}
+        <input
+          type="text"
+          placeholder="Enter Your last Name"
+          {...register("last_name")}
+        />
+        {errors.last_name && <span>{errors.last_name.message}</span>}
+        <input
+          type="email"
+          placeholder="Enter Your Email"
+          {...register("email")}
+        />
+        {errors.email && <span>{errors.email.message}</span>}
+        <input
+          type="password"
+          placeholder="Enter Your Password"
+          {...register("password")}
+        />
+        {errors.password && <span>{errors.password.message}</span>}
+
+        <input
+          type="password"
+          placeholder="Enter Your Confirm Password"
+          {...register("confirm_password")}
+        />
+        {errors.confirm_password && (
+          <span>{errors.confirm_password.message}</span>
+        )}
+
+        <button disabled={isSubmitting} type="submit">
+          Sign Up
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      </form>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
